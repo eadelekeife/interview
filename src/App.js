b25e4f8fd76401e218b6ab0a9a15fe213508adf5
Adeleke Ifeoluwase <eadelekeife@gmail.com>
2023-06-11 13:41:52 -0700

    interview initialization

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchUsers } from './actions/userActions';
+import "./App.css";
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch, connect } from 'react-redux';
+import { fetchUsers } from './actions/userActions';
+import { Table, Input, Select, Spin } from 'antd';
+
+function App(props) {
   const dispatch = useDispatch();
   const { users, loading, error } = useSelector(state => state.users);
 
@@ -10,17 +13,24 @@ function App() {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+
   const [searchTerm, setSearchTerm] = useState('');
   const [genderFilter, setGenderFilter] = useState('all');
   const [currentPage, setCurrentPage] = useState(1);
   const [usersPerPage] = useState(10);
+  const [loadingButton, setloadingButton] = useState(true);
+
+  useEffect(() => {
+    if (users.length) setloadingButton(false)
+  }, [])
 
   const handleChangeSearchTerm = e => {
     setSearchTerm(e.target.value);
   };
 
+
   const handleChangeGenderFilter = e => {
-    setGenderFilter(e.target.value);
+    setGenderFilter(e);
     setCurrentPage(1);
   };
 
@@ -33,71 +43,176 @@ function App() {
   const indexOfLastUser = currentPage * usersPerPage;
   const indexOfFirstUser = indexOfLastUser - usersPerPage;
 
-  const filteredUsers = users.filter(user => {
-    const name = user.name.toLowerCase();
-    const username = user.username.toLowerCase();
+  // const filteredUsers = users.filter(user => {
+  //   const name = user?.name.toLowerCase();
+  //   const username = user?.username.toLowerCase();
+  //   const searchTermLower = searchTerm.toLowerCase();
+  //   const gender = user?.gender.toLowerCase();
+  //   return (
+  //     (name.includes(searchTermLower) || username.includes(searchTermLower)) &&
+  //     (genderFilter === 'all' || genderFilter === gender)
+  //   );
+  // });
+
+  const filterForMe = users.filter(user => {
+    const name = user?.name.toLowerCase();
+    const username = user?.username.toLowerCase();
     const searchTermLower = searchTerm.toLowerCase();
-    const gender = user.gender.toLowerCase();
-    return (
-      (name.includes(searchTermLower) || username.includes(searchTermLower)) &&
-      (genderFilter === 'all' || genderFilter === gender)
-    );
-  });
+    // const gender = user?.gender.toLowerCase();
+    return (name.includes(searchTermLower) || username.includes(searchTermLower))
+    // &&
+    //   (genderFilter === 'all' || genderFilter === gender)
+  })
 
-  const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
+  // const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
 
-  const renderTableHeader = () => {
-    return (
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Username</th>
-          <th>Email</th>
-          <th>Phone</th>
-          <th>Website</th>
-        </tr>
-      </thead>
-    );
-  };
+  // const renderTableHeader = () => {
+  //   return (
+  //     <thead>
+  //       <tr>
+  //         <th>Name</th>
+  //         <th>Username</th>
+  //         <th>Email</th>
+  //         <th>Phone</th>
+  //         <th>Website</th>
+  //       </tr>
+  //     </thead>
+  //   );
+  // };
 
-  const renderTableBody = () => {
-    return (
-      <tbody>
-        {currentUsers.map(user => (
-          <tr key={user.id}>
-            <td>{user.name}</td>
-            <td>{user.username}</td>
-            <td>{user.email}</td>
-            <td>{user.phone}</td>
-            <td>{user.website}</td>
-          </tr>
-        ))}
-      </tbody>
-    );
-  };
+  // const renderTableBody = () => {
+  //   return (
+  //     <tbody>
+  //       {currentUsers.map(user => (
+  //         <tr key={user.id}>
+  //           <td>{user.name}</td>
+  //           <td>{user.username}</td>
+  //           <td>{user.email}</td>
+  //           <td>{user.phone}</td>
+  //           <td>{user.website}</td>
+  //         </tr>
+  //       ))}
+  //     </tbody>
+  //   );
+  // };
 
-  const renderPagination = () => {
-    const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(filteredUsers.length / usersPerPage); i++) {
-      pageNumbers.push(i);
+  // const renderPagination = () => {
+  //   const pageNumbers = [];
+  //   for (let i = 1; i <= Math.ceil(filteredUsers.length / usersPerPage); i++) {
+  //     pageNumbers.push(i);
+  //   }
+  //   return (
+  //     <ul className="pagination">
+  //       {pageNumbers.map(number => (
+  //         <li key={number} className={`page-item ${currentPage === number ? 'active' : ''}`}>
+  //           <button className="page-link" onClick={() => setCurrentPage(number)}>
+  //             {number}
+  //           </button>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   );
+  // };
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Username',
+      dataIndex: 'username',
+      key: 'username',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Phone',
+      dataIndex: 'phone',
+      key: 'phone',
+    },
+    {
+      title: 'Website',
+      dataIndex: 'website',
+      key: 'website',
+    },
+  ];
+  const dataSource = filterForMe.map((user, index) => {
+    return {
+      key: index,
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      phone: user.phone,
+      website: user.website,
     }
-    return (
-      <ul className="pagination">
-        {pageNumbers.map(number => (
-          <li key={number} className={`page-item ${currentPage === number ? 'active' : ''}`}>
-            <button className="page-link" onClick={() => setCurrentPage(number)}>
-              {number}
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
-  };
+  })
+
+  const updateInputBox = e => {
+    setSearchTerm(e.target.value)
+  }
+
   return (
     <div className="App">
-
+      <div className="contain">
+        {
+          loadingButton ?
+            <div>
+              <>
+                <div className="filter-box">
+                  <div>
+                    <Input type="text"
+                      style={{ height: '3rem' }}
+                      value={searchTerm} onChange={updateInputBox} />
+                  </div>
+                  {/* <div>
+                <Select
+                  defaultValue="lucy"
+                  style={{
+                    width: 120,
+                  }}
+                  onChange={handleChangeGenderFilter}
+                  options={[
+                    {
+                      value: 'all',
+                      label: 'All',
+                    },
+                    {
+                      value: 'male',
+                      label: 'Male',
+                    },
+                    {
+                      value: 'female',
+                      label: 'Female',
+                    }
+                  ]}
+                />
+              </div> */}
+                  <div>
+                    <button
+                      onClick={() => handleClearFilters()}
+                    >Clear Input Field</button>
+                  </div>
+                </div>
+                <div className="table-data">
+                  <Table dataSource={dataSource} columns={columns} />;
+                </div>
+              </>
+            </div>
+            :
+            <div>
+              <Spin />
+            </div>
+        }
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+const mapStateToProps = store => {
+  return { appData: store }
+}
+export default connect(mapStateToProps)(App);
--- src/store.js
+++ src/store.js
@@ -1,7 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS, FETCH_USERS_FAILURE } from './actions/userActions';
+
+const defaultStore = {
+  users: [],
+  loading: false,
+  error: ''
+};
+
+const users = (store = defaultStore, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...store,
+        // loading: true
+      };
+    case FETCH_USERS_SUCCESS:
+      return {
+        ...store,
+        users: action.payload
+      };
+    case FETCH_USERS_FAILURE:
+      return {
+        ...store,
+        error: action.payload
+      };
+    default:
+      return store
+  }
+}
 
 const store = configureStore({
-  reducer: {},
+  reducer: { users },
 })
 
 export default store;
